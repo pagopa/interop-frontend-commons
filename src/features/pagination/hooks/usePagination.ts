005ef9feec21a5ac60a8bc2f5298b4bc2ac51948
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-03-24 11:05:42 +0100

    Added JSDocs to the usePagination hook

--- src/features/pagination/hooks/usePagination.ts
+++ src/features/pagination/hooks/usePagination.ts
@@ -1,6 +1,25 @@
 import React from 'react'
 import { useSearchParams } from 'react-router-dom'
 
+/**
+ * @description
+ * This hook is used to manage the pagination state keeping it in sync with the url params.
+ * @param options - An object with a `limit` property used to calculate the current page number.
+ * @returns The pagination params, pagination props to be passed to the `Pagination` component and a function to get the total page count.
+ * @example
+ * const {
+ *   paginationParams,
+ *   paginationProps,
+ *   getTotalPageCount
+ * } = usePagination({ limit: 10 })
+ *
+ * const totalPages = getTotalPageCount(response.totalCount)
+ *
+ * return (
+ *   <Pagination {...paginationProps} totalPages={totalPages} />
+ * )
+ *
+ */
 export function usePagination(options: { limit: number }) {
   const [searchParams, setSearchParams] = useSearchParams()
 
