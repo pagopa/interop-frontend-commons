bbeb8346792cc2f9d3585e4374ccbdffc8d8e220
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-03-25 13:37:09 +0100

    Changed update url search params logic

--- src/features/filters/hooks/useFilters.ts
+++ src/features/filters/hooks/useFilters.ts
@@ -69,93 +69,91 @@ export function useFilters<TFiltersParams extends FiltersParams>(
 
   const onChangeActiveFilter = React.useCallback<FiltersHandler>(
     (type, filterKey, value) => {
-      const newSearchParams = new URLSearchParams(searchParams)
+      setSearchParams((searchParams) => {
+        let shouldBeRemoved = false
+        if (type === 'datepicker' && value === null) {
+          shouldBeRemoved = true
+        }
+        if (
+          ['freetext', 'autocomplete-multiple', 'numeric'].includes(type) &&
+          (value as string | Array<FilterOption>).length === 0
+        ) {
+          shouldBeRemoved = true
+        }
+        if (shouldBeRemoved) {
+          searchParams.delete(filterKey)
+          return searchParams
+        }
 
-      let shouldBeRemoved = false
-      if (type === 'datepicker' && value === null) {
-        shouldBeRemoved = true
-      }
-      if (
-        ['freetext', 'autocomplete-multiple', 'numeric'].includes(type) &&
-        (value as string | Array<FilterOption>).length === 0
-      ) {
-        shouldBeRemoved = true
-      }
-      if (shouldBeRemoved) {
-        newSearchParams.delete(filterKey)
-        setSearchParams(newSearchParams)
-        return
-      }
-
-      switch (type) {
-        case 'numeric':
-        case 'freetext':
-          newSearchParams.set(filterKey, String(value))
-          break
-        case 'autocomplete-multiple':
-          const urlParamMultipleFilterValue = encodeMultipleFilterFieldValue(
-            value as Array<FilterOption>
-          )
-          newSearchParams.set(filterKey, urlParamMultipleFilterValue)
-          break
-        case 'autocomplete-single':
-          const urlParamSingleFilterValue = encodeSingleFilterFieldValue(value as FilterOption)
-          newSearchParams.set(filterKey, urlParamSingleFilterValue)
-          break
-        case 'datepicker':
-          newSearchParams.set(filterKey, (value as Date).toISOString())
-          break
-      }
-
-      newSearchParams.delete('offset')
-      setSearchParams(newSearchParams)
+        switch (type) {
+          case 'numeric':
+          case 'freetext':
+            searchParams.set(filterKey, String(value))
+            break
+          case 'autocomplete-multiple':
+            const urlParamMultipleFilterValue = encodeMultipleFilterFieldValue(
+              value as Array<FilterOption>
+            )
+            searchParams.set(filterKey, urlParamMultipleFilterValue)
+            break
+          case 'autocomplete-single':
+            const urlParamSingleFilterValue = encodeSingleFilterFieldValue(value as FilterOption)
+            searchParams.set(filterKey, urlParamSingleFilterValue)
+            break
+          case 'datepicker':
+            searchParams.set(filterKey, (value as Date).toISOString())
+            break
+        }
+        searchParams.delete('offset')
+        return searchParams
+      })
     },
-    [searchParams, setSearchParams]
+    [setSearchParams]
   )
 
   const onRemoveActiveFilter = React.useCallback<FiltersHandler>(
     (type, filterKey, value) => {
-      const newSearchParams = new URLSearchParams(searchParams)
-
-      switch (type) {
-        case 'freetext':
-        case 'numeric':
-        case 'datepicker':
-        case 'autocomplete-single':
-          newSearchParams.delete(filterKey)
-          break
-        case 'autocomplete-multiple':
-          const urlParamsValue = searchParams.get(filterKey)
-          if (urlParamsValue) {
-            const values = decodeMultipleFilterFieldValue(urlParamsValue)
-            const filteredValues = values.filter((option) => option.value !== value)
-            if (filteredValues.length === 0) {
-              newSearchParams.delete(filterKey)
-            } else {
-              newSearchParams.set(filterKey, encodeMultipleFilterFieldValue(filteredValues))
+      setSearchParams((searchParams) => {
+        switch (type) {
+          case 'freetext':
+          case 'numeric':
+          case 'datepicker':
+          case 'autocomplete-single':
+            searchParams.delete(filterKey)
+            break
+          case 'autocomplete-multiple':
+            const urlParamsValue = searchParams.get(filterKey)
+            if (urlParamsValue) {
+              const values = decodeMultipleFilterFieldValue(urlParamsValue)
+              const filteredValues = values.filter((option) => option.value !== value)
+              if (filteredValues.length === 0) {
+                searchParams.delete(filterKey)
+              } else {
+                searchParams.set(filterKey, encodeMultipleFilterFieldValue(filteredValues))
+              }
             }
-          }
-          break
-      }
-
-      newSearchParams.delete('offset')
-      setSearchParams(newSearchParams)
+            break
+        }
+        searchParams.delete('offset')
+        return searchParams
+      })
     },
-    [searchParams, setSearchParams]
+    [setSearchParams]
   )
 
   const onResetActiveFilters = React.useCallback(() => {
-    const newSearchParams = new URLSearchParams(searchParams)
-    const paramKeys = [...newSearchParams.keys()]
-    paramKeys.forEach((paramKey) => {
-      // Only delete the params that are related to filters
-      const isFilterKey = fields.some((field) => field.name === paramKey)
-      if (isFilterKey) {
-        newSearchParams.delete(paramKey)
-      }
+    setSearchParams((searchParams) => {
+      const paramKeys = [...searchParams.keys()]
+      paramKeys.forEach((paramKey) => {
+        // Only delete the params that are related to filters
+        const isFilterKey = fields.some((field) => field.name === paramKey)
+        if (isFilterKey) {
+          searchParams.delete(paramKey)
+        }
+      })
+      return searchParams
     })
-    setSearchParams(newSearchParams)
-  }, [searchParams, setSearchParams, fields])
+  }, [setSearchParams, fields])
 
   const { activeFilters, filtersParams } = mapSearchParamsToActiveFiltersAndFilterParams(
     searchParams,
