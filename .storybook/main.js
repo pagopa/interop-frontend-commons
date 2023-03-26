5fdef094b943b27d39fce1eff2e8212f64192432
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-03-26 14:19:19 +0200

    Added path alias resolving to storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const { mergeConfig } = require('vite')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -11,5 +14,12 @@ module.exports = {
   },
   features: {
     storyStoreV7: true,
+  },
+  async viteFinal(config, { configType }) {
+    return mergeConfig(config, {
+      resolve: {
+        alias: { '@': path.resolve(path.dirname(__dirname), 'src') },
+      },
+    })
   },
 }
