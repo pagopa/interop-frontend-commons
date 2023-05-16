ecfa3440b1ab74683a67bc571aa52ab41c19f8cd
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-05-16 09:56:13 +0200

    Added more snapshot tests to Spinner

--- src/components/__tests__/Spinner.test.tsx
+++ src/components/__tests__/Spinner.test.tsx
@@ -13,6 +13,18 @@ describe('Spinner component', () => {
     expect(tree).toMatchSnapshot()
   })
 
+  it('Renders correctly with label and column-reverse direction', () => {
+    const tree = renderer
+      .create(<Spinner direction="column-reverse" label={'test-title'} />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('Renders correctly without label and column-reverse direction', () => {
+    const tree = renderer.create(<Spinner direction="column-reverse" />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
   it('Renders correctly with label and row direction', () => {
     const tree = renderer.create(<Spinner direction="row" label={'test-title'} />).toJSON()
     expect(tree).toMatchSnapshot()
@@ -20,6 +32,16 @@ describe('Spinner component', () => {
 
   it('Renders correctly without label and row direction', () => {
     const tree = renderer.create(<Spinner direction="row" />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('Renders correctly with label and row-reverse direction', () => {
+    const tree = renderer.create(<Spinner direction="row-reverse" label={'test-title'} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('Renders correctly without label and row-reverse direction', () => {
+    const tree = renderer.create(<Spinner direction="row-reverse" />).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
